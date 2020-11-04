c362dc2b57b2ae815e8c412cd813649e3db41790
rc-rohan <rohan6hawat@gmail.com>
2020-11-05 02:14:23 +0530

    Added basic styling to header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,25 +9,30 @@ const Header = () => {
   const [movie, setMovie] = useState();
 
   // todo:set interval for the repetative call of the trending movie
-  // let movieIndex = 1;
-  // setInterval(() => {
-  //   if(movieIndex<trending.length){
-  //     setMovie(trending[movieIndex])
-  //     movieIndex++;
-  //     console.log(movie,movieIndex)
-  //   }else{
-  //     movieIndex=0;
-  //     setMovie(trending[movieIndex])
-  //   }
-  // }, 5000);
+  let movieIndex = 1;
+  setInterval(() => {
+    // if(movieIndex<trending?.length){
+    //   setMovie(trending[movieIndex])
+    //   movieIndex++;
+    //   console.log(movie,movieIndex)
+    //   console.log(trending)
+    // }else{
+    //   movieIndex=0;
+    //   setMovie(trending[movieIndex])
+    // }
+    // if(trending?.length)
+    //   setMovie(trending[movieIndex])
+      movieIndex++;
+      console.log( movieIndex);
+  }, 5000);
 
   const fetchData = async () => {
-    const { data } = await axios.get(requests.fetchTrending);
+    const { data } = await axios.get(`${requests.baseTmdbURL}${requests.fetchTrending}`);
     setTrending(data.results);
     // unable to set the movies
     setMovie(trending[0]);
-    console.log(trending);
-    console.log(movie);
+    // console.log(trending)
+    // console.log(data.results);
   };
   useEffect(() => {
     fetchData();
@@ -41,19 +46,33 @@ const Header = () => {
     <header
       className="banner"
       style={{
-        background: `linear-gradient(rgba(0,0,0,.1),rgba(0,0,0,.9)),
-            url(${requests.imageURL}${trending[0]?.backdrop_path})
+        background: `linear-gradient(rgba(0,0,0,.5),rgba(0,0,0,.9)) ,
+            url(${requests.baseImageURL}${trending[0]?.backdrop_path})
             left top/cover`,
       }}
     >
-      <h1 className="banner__name">{trending[0]?.original_title}</h1>
-      <div className="banner__buttons">
-        <button className="btn banner__details-page" onClick={() => handleClick()}>
-          See Details
-        </button>
-        <button className="btn banner__trailer-link" onClick={()=>{}}>Youtube Trailer</button>
+      <div className="banner__content">
+        <div>
+          <h1 className="banner__name">{trending[0]?.original_title}</h1>
+          <div className="banner__buttons">
+            <button
+              className="btn banner__details-page"
+              onClick={() => handleClick()}
+            >
+              See Details
+            </button>
+            <button className="btn banner__trailer-link" onClick={() => {}}>
+              Trailer
+            </button>
+          </div>
+          <p className="banner__description">{trending[0]?.overview}</p>
+        </div>
+        <img
+          className='banner__image'
+          src={`${requests.baseImageURL}${trending[0]?.poster_path}`}
+          alt=""
+        />
       </div>
-      <p className="banner__description">{trending[0]?.overview}</p>
     </header>
   );
 };
--- src/requests.js
+++ src/requests.js
@@ -6,17 +6,16 @@ const requests = {
   youtubeURL: `https://www.youtube.com/watch?v=`,
   baseImageURL: `https://image.tmdb.org/t/p/original/`,
   baseTmdbURL: `https://api.themoviedb.org/3`,
-  fetchTrending: `/trending/all/week?api_key=${API_KEY}`,
+  fetchTrending: `/trending/movie/week?api_key=${API_KEY}`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&sort_by=popularity.desc&page=1&with_networks=213&include_null_first_air_dates=false`,
   fetchPrimeVideos: `/discover/tv?api_key=${API_KEY}&sort_by=popularity.desc&with_networks=1024`,
   fetchDisneyHotstar: `/discover/tv?api_key=${API_KEY}&sort_by=popularity.desc&with_networks=3919`,
 
-  
   // fetch specfic data on another page
-  fetchMultiQuery: `/search/multi?api_key=${API_KEY}&query=${query}`,
+  // fetchMultiQuery: `/search/multi?api_key=${API_KEY}&query=${query}`,
   // this 2 will be used in the specific pages
-  fetchMovieDetails: `/movie/${movie_id}?api_key=${API_KEY}&append_to_response=videos,images`,
-  fetchTVDetails: `/tv/${tv_id}?api_key=${API_KEY}&append_to_response=videos,images`,
+  // fetchMovieDetails: `/movie/${movie_id}?api_key=${API_KEY}&append_to_response=videos,images`,
+  // fetchTVDetails: `/tv/${tv_id}?api_key=${API_KEY}&append_to_response=videos,images`,
 };
 
 
